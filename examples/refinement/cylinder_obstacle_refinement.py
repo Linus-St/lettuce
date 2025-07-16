510523b6f47aec69cb1a070654b38c994488b130
Linus-St <105305977+Linus-St@users.noreply.github.com>
2025-07-16 20:13:34 +0200

    Fixed Grid alignment and vtk_reporters in Cylinder Example

--- examples/refinement/cylinder_obstacle_refinement.py
+++ examples/refinement/cylinder_obstacle_refinement.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
+name = './data/vtk_debugging/multi_cylinder_1000_steps'
+
 def map_fine_on_coarse(fine: np.array, coarse: np.array, offset):
     result = coarse.repeat(2, 0).repeat(2, 1)
     area_fine_x = slice(offset[0]*2, offset[0]*2+fine.shape[0])
@@ -28,10 +30,8 @@ physical_length = 10
 physical_width = 5
 
 ref_lvl_1_pu = np.array([[1/10, 3/10], [5/10, 7/10]])
-ref_lvl_1_length_pu = int((ref_lvl_1_pu[1][0]-ref_lvl_1_pu[0][0]) * physical_length)
 
 ref_lvl_2_pu = np.array([[2/10, 4/10], [4/10, 6/10]])
-ref_lvl_2_length_pu = int((ref_lvl_2_pu[1][0]-ref_lvl_2_pu[0][0]) * physical_length)
 
 resolution = [400, 200]
 
@@ -44,22 +44,26 @@ flow_lvl0 = Obstacle(context, resolution, reynolds_number=reynolds, mach_number=
 flow_lvl0.boundaries[2] = None
 
 refinement_lvl_1 = refinement_config.refinement_levels[0]
-resolution_lvl1 = [int(i)*2-1 for i in refinement_lvl_1.border_length_coarse]
+resolution_lvl1 = refinement_lvl_1.resolution
+char_len_lu_1 = flow_lvl0.char_length_lu*2
+ref_lvl_1_length_pu = resolution_lvl1[0] / char_len_lu_1
 flow_lvl1 = Obstacle(context, list(resolution_lvl1), reynolds_number=reynolds, mach_number=mach, domain_length_x=ref_lvl_1_length_pu,
                      ref_level=1, start_point=refinement_lvl_1.minimum_point_lvl0, end_point=refinement_lvl_1.maximum_point_lvl0)
-flow_lvl1.char_length_lu = flow_lvl0.char_length_lu*2-1
+flow_lvl1.char_length_lu = char_len_lu_1
 flow_lvl1.boundaries[0] = None
 flow_lvl1.boundaries[1] = None
 flow_lvl1.boundaries[2] = None
 
 refinement_lvl_2 = refinement_config.refinement_levels[1]
-resolution_lvl2 = [int(i)*2-1 for i in refinement_lvl_2.border_length_coarse]
+resolution_lvl2 = refinement_lvl_2.resolution
+char_len_lu_2 = flow_lvl1.char_length_lu*2
+ref_lvl_2_length_pu = resolution_lvl2[0] / char_len_lu_2
 flow_lvl2 = Obstacle(context, list(resolution_lvl2), reynolds_number=reynolds, mach_number=mach, domain_length_x=ref_lvl_2_length_pu,
                      ref_level=2, start_point=refinement_lvl_2.minimum_point_lvl0, end_point=refinement_lvl_2.maximum_point_lvl0)
-flow_lvl2.char_length_lu = flow_lvl1.char_length_lu*2-1
+flow_lvl2.char_length_lu = char_len_lu_2
 flow_lvl2.boundaries[0] = None
 flow_lvl2.boundaries[1] = None
-# TODO boundaries nicht übere indices
+# TODO boundaries nicht über indices
 
 x, y = torch.meshgrid(torch.arange(refinement_lvl_2.border_length_coarse[0]*2-1), torch.arange(refinement_lvl_2.border_length_coarse[1]*2-1), indexing='ij')
 r = .25*y.max()
@@ -71,21 +75,11 @@ flow_lvl2.mask = ((x - x_c) ** 2 + (y - y_c) ** 2) < (r ** 2)
 collision_lvl0= BGKCollision(tau=flow_lvl0.units.relaxation_parameter_lu)
 simulation_lvl0 = Simulation(flow_lvl0, collision_lvl0, refinement=refinement_lvl_1, reporter=[])
 
-name = './data/vtk_debugging/multi_cylinder_with_mask/'
-
-VTKreport0 = lt.VTKReporter(interval=25, filename_base=name+'lvl0', flow_grid=flow_lvl0.grid)
-simulation_lvl0.reporter.append(VTKreport0)
-
 collision_lvl1= BGKCollision(tau=flow_lvl1.units.relaxation_parameter_lu)
 simulation_lvl1 = Simulation(flow_lvl1, collision_lvl1, refinement=refinement_lvl_2, reporter=[])
-VTKreport1 = lt.VTKReporter(interval=50, filename_base=name+'lvl1', flow_grid=flow_lvl1.grid)
-simulation_lvl0.reporter.append(VTKreport1)
 
 collision_lvl2= BGKCollision(tau=flow_lvl2.units.relaxation_parameter_lu)
 simulation_lvl2 = Simulation(flow_lvl2, collision_lvl1, reporter=[])
-VTKreport2 = lt.VTKReporter(interval=100, filename_base=name+'lvl2', flow_grid=flow_lvl2.grid)
-VTKreport2.output_mask(simulation_lvl2)
-simulation_lvl0.reporter.append(VTKreport2)
 energyreporter = lt.ObservableReporter(lt.IncompressibleKineticEnergy(flow_lvl2), interval=50)
 simulation_lvl2.reporter.append(energyreporter)
 
@@ -95,7 +89,9 @@ refinement_lvl_1.fine_simulation = simulation_lvl1
 refinement_lvl_2.coarse_simulation = simulation_lvl1
 refinement_lvl_2.fine_simulation = simulation_lvl2
 
-simulation_lvl0(100)
+refinement_config.add_vtk_reporters(name, 25)
+
+simulation_lvl0(1000)
 
 u_0 = context.convert_to_ndarray(flow_lvl0.u_pu)
 u_0_norm= np.linalg.norm(u_0, axis=0).transpose()
