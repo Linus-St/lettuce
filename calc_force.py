37306a6302ede1aca1388c26fbf495fcfcf99239
linus <105305977+Linus-St@users.noreply.github.com>
2025-08-10 22:05:49 +0200

    calc force from csv

---
+++ calc_force.py
@@ -0,0 +1,34 @@
+import numpy
+import pandas as pd
+import numpy as np
+import scipy.signal as s
+
+name = ""
+
+def import_csv(filename):
+    return pd.read_csv(filename, sep=" ", names=["step", "time", "drag", "lift"])
+
+def calc_drag(data):
+    return numpy.average(data)
+
+def calc_lift(data):
+    width = 10
+    peaks = s.find_peaks(data, width=width)[0]
+    valleys = s.find_peaks(-data, width=width)[0]
+    p = data[peaks]
+    v = -data[valleys]
+    return numpy.average(np.append(data[peaks], -data[valleys]))
+
+df = import_csv(name)
+time = df["time"].to_numpy()
+drag = df["drag"].to_numpy()
+lift = df["lift"].to_numpy()
+
+indices = np.where(time > 50)
+
+print("Drag: ", calc_drag(drag[indices]))
+print("Lift: ", calc_lift(lift[indices]))
+
+
+
+
