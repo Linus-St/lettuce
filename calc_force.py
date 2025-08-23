941d17221e13030c6ec170b672e3a0b2d8ebdf17
linus <105305977+Linus-St@users.noreply.github.com>
2025-08-23 18:16:02 +0200

    calc and lift calculation expanded to handle directories of result files / calculate diff from control set

--- calc_force.py
+++ calc_force.py
@@ -1,9 +1,12 @@
+import os
+import json
+
 import numpy
 import pandas as pd
 import numpy as np
 import scipy.signal as s
 
-name = ""
+directory = ""
 
 def import_csv(filename):
     return pd.read_csv(filename, sep=" ", names=["step", "time", "drag", "lift"])
@@ -19,19 +22,54 @@ def calc_lift(data):
     v = -data[valleys]
     return numpy.average(np.append(data[peaks], -data[valleys]))
 
-def calculate(file):
+def calculate(file, t):
     df = import_csv(file)
     time = df["time"].to_numpy()
     drag = df["drag"].to_numpy()
     lift = df["lift"].to_numpy()
 
-    indices = np.where(time > 80)
+    indices = np.where(time > t)
 
-    print("Drag: ", calc_drag(drag[indices]))
-    print("Lift: ", calc_lift(lift[indices]))
+    drag = calc_drag(drag[indices])
+    lift = calc_lift(lift[indices])
+
+    print("Drag: ", drag)
+    print("Lift: ", lift)
+    return drag, lift
+
+def read_directory(name):
+    return os.listdir(name)
+
+def calculate_percentage_diff(control, actual):
+    return abs(actual - control) * 100 / control
 
 def main():
+    time = 65
+    tests = sorted(read_directory(directory))
+
+    drag_values = dict()
+    lift_values = dict()
+    drag_control, lift_control = None, None
+
+    for test in tests:
+        if test == "control":
+            drag_control, lift_control = calculate(os.path.join(directory, test, "control", "drag_lift.csv"), time)
+        elif test == "results":
+            pass
+        else:
+            drag, lift = calculate(os.path.join(directory, test, "multi_refined", "drag_lift.csv"), time)
+            drag_values[test] = drag
+            lift_values[test] = lift
+
+    for key, value in drag_values.items():
+        drag_values[key] = [value, calculate_percentage_diff(drag_control, value)]
+    for key, value in lift_values.items():
+        lift_values[key] = [value, calculate_percentage_diff(lift_control, value)]
+
+    drag_values["control"] = drag_control
+    lift_values["control"] = lift_control
     return
+
 if __name__ == '__main__':
     main()
 
