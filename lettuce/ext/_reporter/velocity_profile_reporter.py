5dee11518eb4687185e121a4d7c6e38bea8f2548
linus <105305977+Linus-St@users.noreply.github.com>
2025-09-19 11:10:56 +0200

    velocity profile reporter logic implemented

--- lettuce/ext/_reporter/velocity_profile_reporter.py
+++ lettuce/ext/_reporter/velocity_profile_reporter.py
@@ -1,3 +1,4 @@
+import math
 import os
 from abc import ABC, abstractmethod
 
@@ -23,7 +24,7 @@ class XGenerator(ABC):
 
 class VelocityProfileReporter(Reporter):
 
-    def __init__(self, directory, diameter, y_span, xgenerator, begin_at, interval=1):
+    def __init__(self, directory, diameter, y_span, y_len, xgenerator, begin_at, interval=1):
         Reporter.__init__(self, interval)
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -31,16 +32,28 @@ class VelocityProfileReporter(Reporter):
         self.dir_x = os.path.join(self.directory, 'x')
         self.dir_y = os.path.join(self.directory, 'y')
         self.d = diameter
-        self.y = diameter * y_span #bekomme ich hier raus direkt ein slice?
-        self.y_slice = None
-        self.xvalues = xgenerator.generate()
+        #TODO testen ob übereinstimmung mit center of diameter
+        self.y_slice, self.y_d = self.setup_y(y_len, y_span)
+        self.x_d = xgenerator.generate()
         self.begin_at = begin_at
-        # hier schon die files schreiben und 1 - 2 header zeilen? y_lu, y/D
+        np.savetxt(os.path.join(self.directory, 'x_values'), np.array(self.x_d))
+        np.savetxt(os.path.join(self.directory, 'y_values'), np.array(self.y_d))
+        return
+
+    def setup_y(self, y_len, y_span):
+        mid = (y_len-1) / 2
+        y_diff = self.d * y_span
+        if mid + y_diff < y_len:
+            y_slice = slice(math.ceil(mid - y_diff), math.floor(mid + y_diff) + 1)
+        else:
+            y_slice = slice(0)
+        y_d = [(i - mid) / self.d for i in range (y_slice.start, y_slice.stop)]
+        return y_slice, y_d
 
     def __call__(self, simulation: 'Simulation'):
         if simulation.flow.i >= self.begin_at:
             u = simulation.flow.u()
-            y_values = u[:, self.xvalues, self.y_slice]
+            y_values = u[:, self.x_d, self.y_slice]
             self.save(y_values, simulation.flow.i)
         return
 
